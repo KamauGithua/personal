151dfc84cc79bf41b6ec4cbe68347558f654361e
kamau <githuakamau99@gmail.com>
2024-09-11 11:52:16 +0300

    Added Testimonial file

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import Placard from "./components/placard/Placard";
 import Features from "./components/features/Features";
 import Projects from "./components/projects/Projects";
 import Resume from "./components/resume/Resume";
+import Testimonial from "./components/testimonial/Testimonial";
 
 
 function App() {
   return (
-    <div className="w-full h-auto bg-bodyColor text-lightText">
-       <div className="max-w-screen-xl mx-auto px-16">
+    <div className="w-full h-auto bg-bodyColor text-lightText px-4">
        <Navbar />
+       <div className="max-w-screen-xl mx-auto px-16">
        <Placard />
        <Features />
        <Projects />
        <Resume />
+       <Testimonial />
        
        </div>
     </div>
